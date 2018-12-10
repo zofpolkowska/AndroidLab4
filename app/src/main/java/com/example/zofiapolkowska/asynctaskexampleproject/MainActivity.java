9d27527aa18a4a83b33fd18af86a7510698686de
zofpolkowska <zofpolkowska@gmail.com>
2018-12-10 19:25:07 +0100

    Add gui elements

---
+++ app/src/main/java/com/example/zofiapolkowska/asynctaskexampleproject/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zofiapolkowska.asynctaskexampleproject;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void decimalToBinary(View view){
+        // TODO: "Obliczenia" powinny być wykonane w tle
+        //       Warto użyć Thread.sleep(1000) aby chwilę trwały :)
+
+    }
+
+
+}
